ad579138562f703b789a1d85268807847153280b
MilagrosT36 <F20LAB207E06@host.docker.internal>
2026-04-24 09:59:35 -0500

    "Ingreso al alumno"

---
+++ src/main/java/modelo/Alumno.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author F20LAB207E06
+ */
+public class Alumno {
+    
+}
