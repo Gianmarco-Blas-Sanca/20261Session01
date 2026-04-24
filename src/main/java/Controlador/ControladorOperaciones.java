23149953989eb8c35d5cb1d43d44b2946333429d
MilagrosT36 <F20LAB207E06@host.docker.internal>
2026-04-24 11:34:03 -0500

    botones

---
+++ src/main/java/Controlador/ControladorOperaciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+import modelo.Operacion;
+import vista.frmOperaciones;
+/**
+ *
+ * @author F20LAB207E06
+ */
+public class ControladorOperaciones {
+    private Operacion modelo;
+    private frmOperaciones 
+    
+}
