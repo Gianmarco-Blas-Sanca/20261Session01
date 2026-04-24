2858a5afb8a19dd673f5d065bb9bbb8eafc5be15
MilagrosT36 <F20LAB207E06@host.docker.internal>
2026-04-24 11:47:22 -0500

    ultimos camnbios

--- src/main/java/Controlador/ControladorOperaciones.java
+++ src/main/java/Controlador/ControladorOperaciones.java
@@ -11,6 +11,21 @@ import vista.frmOperaciones;
  */
 public class ControladorOperaciones {
     private Operacion modelo;
-    private frmOperaciones 
+    private frmOperaciones vista;
+    public ControladorOperaciones(Operacion modelo,frmOperaciones vista){
+        this.modelo=modelo;
+        this.vista=vista;
+        this.vista.btnSumar.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e){
+            modelo.setOperador1(Integer.parseInt(vista.txtOperador1.getText()));
+            modelo.setOperador2(Integer.parseInt(vista.txtOperador2.getText()));
+            vista.lblResultado.setText(String.valueOf(modelo.sumar()));
+        }
+        });
+        
+        
+        
+    }
     
 }
